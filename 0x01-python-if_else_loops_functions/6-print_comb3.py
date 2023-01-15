74ec46e617016cc159cdd51b8a7a696fbe219c94
vixthor <tega2112@gmail.com>
2023-01-15 06:26:41 -0800

    6-comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for x in range(0 + i, 10):
+for i in range(0, 8):
+    for x in range(1 + i, 10):
         print("{:d}{:d}".format(i,x), end=', ')
 print("{:d}{:d}".format(i + 1, x))
